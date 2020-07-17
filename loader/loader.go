a6caa25fb6e4df34ec26c5f2734b918900f0de92
Yuri Harrison <yuri.harrison15@gmail.com>
2020-07-16 21:24:42 -0300

    loader

---
+++ loader/loader.go
@@ -0,0 +1,52 @@
+package loader
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	db "github.com/yuriharrison/bexs-test/flightsdb"
+)
+
+const bufferSize = 4096
+
+// ImportFlightsFromFile import flights into FlightsDB
+func ImportFlightsFromFile(fileName string, db *db.FlightsDB) error {
+	f, err := os.OpenFile(fileName, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	err = LoadFlights(f, db)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// LoadFlights load flights into FlightsDB
+func LoadFlights(file io.Reader, db *db.FlightsDB) error {
+	csvReader := csv.NewReader(file)
+	for {
+		row, err := csvReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		if len(row) != 3 {
+			return fmt.Errorf("Expected 3 columns in CSV got %v", len(row))
+		}
+		origCode, destCode, priceStr := row[0], row[1], row[2]
+		if price, err := strconv.ParseFloat(priceStr, 64); err == nil {
+			err = db.Add(origCode, destCode, price)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
---
+++ loader/loader_test.go
@@ -0,0 +1,23 @@
+package loader
+
+import (
+	"testing"
+
+	flightsdb "github.com/yuriharrison/bexs-test/flightsdb"
+)
+
+const (
+	flightsFile = "./fixtures/flights.csv"
+	numFlights  = 10 // 10 flights 3 updates
+)
+
+func TestImportFlightsFromFile(t *testing.T) {
+	db := flightsdb.NewFlightsDB()
+	err := ImportFlightsFromFile(flightsFile, db)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if db.Size() != numFlights {
+		t.Errorf("Size %v != %v numFlights", db.Size(), numFlights)
+	}
+}
